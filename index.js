5b5bffcabab382652e3635f237f7c2d10a6dead9
sarmad1995 <azaabudeen@gmail.com>
2019-11-24 15:43:15 +0530

    refactor/gitignore

--- index.js
+++
@@ -1,7 +0,0 @@
-import ContentLoader from "./src/ContentLoader";
-import Facebook from "./src/FacebookLoader";
-import Instagram from "./src/InstagramLoader";
-import Bullets from "./src/Bullets";
-
-export default ContentLoader;
-export { Facebook, Instagram, Bullets };
--- lib/index.d.ts
+++
@@ -1,6 +0,0 @@
-import ContentLoader from './ContentLoader';
-import FacebookLoader from './FacebookLoader';
-import InstagramLoader from './InstagramLoader';
-import Bullets from './Bullets';
-export default ContentLoader;
-export { FacebookLoader, Bullets, InstagramLoader };
